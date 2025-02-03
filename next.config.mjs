3745a21b7eb4a8ba5b178657d01127358beb8674
Ju-MINJAE <juminjae98@gmail.com>
2025-02-03 14:52:08 +0900

    Set up GitHub Pages for Next.js project with pnpm

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
