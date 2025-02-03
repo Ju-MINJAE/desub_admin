adcc34fe446c0a64489fa9c2157dc6ef0147fe07
Ju-MINJAE <juminjae98@gmail.com>
2025-02-03 15:21:12 +0900

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@
  */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/desub_admin' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/desub_admin' : '',
 };
 
 export default nextConfig;
