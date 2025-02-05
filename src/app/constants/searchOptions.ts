e9826557010e955af063cb006781d32cc1fd609f
Ju-MINJAE <juminjae98@gmail.com>
2025-02-05 13:18:49 +0900

    feat: Add reviewSearchOptions

--- src/app/constants/searchOptions.ts
+++ src/app/constants/searchOptions.ts
@@ -1,46 +1,65 @@
-import { Cancellation } from "@/types/cancellation";
-import { SearchOption, Subscriber } from "@/types/subscriber";
+import { Cancellation } from '@/types/cancellation';
+import { Review } from '@/types/review';
+import { SearchOption, Subscriber } from '@/types/subscriber';
 
 export const cancellationSearchOptions: SearchOption<Cancellation>[] = [
-  { value: "name", label: "이름", inputType: "text" },
-  { value: "email", label: "이메일", inputType: "text" },
-  { value: "phone", label: "전화번호", inputType: "text" },
-  { value: "cancelDate", label: "취소일자", inputType: "date" },
+  { value: 'name', label: '이름', inputType: 'text' },
+  { value: 'email', label: '이메일', inputType: 'text' },
+  { value: 'phone', label: '전화번호', inputType: 'text' },
+  { value: 'cancelDate', label: '취소일자', inputType: 'date' },
   {
-    value: "cancelReason",
-    label: "취소사유",
-    inputType: "select",
+    value: 'cancelReason',
+    label: '취소사유',
+    inputType: 'select',
     options: [
-      { value: "가격이 비싸서", label: "가격이 비싸서" },
+      { value: '가격이 비싸서', label: '가격이 비싸서' },
       {
-        value: "퀄리티가 마음에 들지 않아서",
-        label: "퀄리티가 마음에 들지 않아서",
+        value: '퀄리티가 마음에 들지 않아서',
+        label: '퀄리티가 마음에 들지 않아서',
       },
-      { value: "소통이 느려서", label: "소통이 느려서" },
+      { value: '소통이 느려서', label: '소통이 느려서' },
       {
-        value: "정직원을 구하는 것이 더 편해서",
-        label: "정직원을 구하는 것이 더 편해서",
+        value: '정직원을 구하는 것이 더 편해서',
+        label: '정직원을 구하는 것이 더 편해서',
       },
-      { value: "회사 예산이 줄어들어서", label: "회사 예산이 줄어들어서" },
-      { value: "기타", label: "기타" },
+      { value: '회사 예산이 줄어들어서', label: '회사 예산이 줄어들어서' },
+      { value: '기타', label: '기타' },
     ],
   },
 ];
 
 export const subscriberSearchOptions: SearchOption<Subscriber>[] = [
-  { value: "name", label: "이름", inputType: "text" },
-  { value: "email", label: "이메일", inputType: "text" },
-  { value: "phone", label: "전화번호", inputType: "text" },
+  { value: 'name', label: '이름', inputType: 'text' },
+  { value: 'email', label: '이메일', inputType: 'text' },
+  { value: 'phone', label: '전화번호', inputType: 'text' },
   {
-    value: "status",
-    label: "구독현황",
-    inputType: "select",
+    value: 'status',
+    label: '구독현황',
+    inputType: 'select',
     options: [
-      { value: "진행중", label: "진행중" },
-      { value: "일시정지", label: "일시정지" },
+      { value: '진행중', label: '진행중' },
+      { value: '일시정지', label: '일시정지' },
     ],
   },
-  { value: "startDate", label: "최초결제일", inputType: "date" },
-  { value: "endDate", label: "최근결제일", inputType: "date" },
-  { value: "expiryDate", label: "구독만료일", inputType: "date" },
+  { value: 'startDate', label: '최초결제일', inputType: 'date' },
+  { value: 'endDate', label: '최근결제일', inputType: 'date' },
+  { value: 'expiryDate', label: '구독만료일', inputType: 'date' },
+];
+
+export const reviewSearchOptions: SearchOption<Review>[] = [
+  { value: 'name', label: '이름', inputType: 'text' },
+  { value: 'email', label: '이메일', inputType: 'text' },
+  { value: 'phone', label: '전화번호', inputType: 'text' },
+  {
+    value: 'reviewRating',
+    label: '별점',
+    inputType: 'select',
+    options: [
+      { value: '1', label: '1점' },
+      { value: '2', label: '2점' },
+      { value: '3', label: '3점' },
+      { value: '4', label: '4점' },
+      { value: '5', label: '5점' },
+    ],
+  },
 ];
