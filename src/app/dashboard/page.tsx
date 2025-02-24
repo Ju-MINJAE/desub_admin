6a3361049b12e12e75c0d0b7cb107ffca5f6121f
Ju-MINJAE <juminjae98@gmail.com>
2025-02-25 01:32:49 +0900

    feat: Connect Dashboard API

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,27 @@
+'use client';
+
 import Image from 'next/image';
 import { Heading } from '../components/ui/Heading';
 import Link from 'next/link';
+import { getAccessToken } from '@/actions/auth/getAccessToken';
+import { useEffect, useState } from 'react';
+import { dashboardAdmin } from '@/api/dashboard';
+import { DashboardResponse } from '@/types/dashboard';
 
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState<DashboardResponse | null>(null);
+
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      const { accessToken } = await getAccessToken();
+      if (!accessToken) return;
+      const data = await dashboardAdmin(accessToken);
+      setDashboardData(data);
+    };
+
+    fetchDashboardData();
+  }, []);
+
   return (
     <div className="flex min-h-screen pl-[30rem] xl:pl-[36rem] pt-[6rem] xl:pt-[11.9rem]">
       <div className="h-[84.5rem] overflow-auto">
@@ -17,15 +36,21 @@ const Dashboard = () => {
             <div className="space-y-[2.6rem]">
               <div className="flex justify-between items-center">
                 <Heading tag="h2">전체 구독</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.total_subscriptions}
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">신규 구독</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.new_subscriptions_today}
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">오늘 구독 일시정지</Heading>
-                <span className="font-bold text-[1.8rem]">3</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.paused_subscriptions}
+                </span>
               </div>
             </div>
           </div>
@@ -40,11 +65,11 @@ const Dashboard = () => {
             <div className="space-y-[2.6rem]">
               <div className="flex justify-between items-center">
                 <Heading tag="h2">전체 취소</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">{dashboardData?.subs_cancel_all}</span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">오늘 취소</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">{dashboardData?.subs_cancel_today}</span>
               </div>
             </div>
           </div>
@@ -59,11 +84,11 @@ const Dashboard = () => {
             <div className="space-y-[2.6rem]">
               <div className="flex justify-between items-center">
                 <Heading tag="h2">전체 리뷰</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">{dashboardData?.all_reviews}</span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">신규 리뷰</Heading>
-                <span className="font-bold text-[1.8rem]">0</span>
+                <span className="font-bold text-[1.8rem]">{dashboardData?.new_reviews}</span>
               </div>
             </div>
           </div>
@@ -78,15 +103,21 @@ const Dashboard = () => {
             <div className="space-y-[2.6rem]">
               <div className="flex justify-between items-center">
                 <Heading tag="h2">전체 고객 수</Heading>
-                <span className="font-bold text-[1.8rem] text-[#e90000]">0</span>
+                <span className="font-bold text-[1.8rem] text-[#e90000]">
+                  {dashboardData?.total_customers}
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">오늘 가입</Heading>
-                <span className="font-bold text-[1.8rem] text-[#e90000]">0</span>
+                <span className="font-bold text-[1.8rem] text-[#e90000]">
+                  {dashboardData?.new_customers_today}
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">오늘 탈퇴</Heading>
-                <span className="font-bold text-[1.8rem]">3</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.deleted_customers_today}
+                </span>
               </div>
             </div>
           </div>
@@ -101,15 +132,21 @@ const Dashboard = () => {
             <div className="space-y-[2.6rem]">
               <div className="flex justify-between items-center">
                 <Heading tag="h2">당월 매출</Heading>
-                <span className="font-bold text-[1.8rem]">80,000,000원</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.monthly_sales.toLocaleString()}원
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">당월 취소 매출</Heading>
-                <span className="font-bold text-[1.8rem]">-2,590,000원</span>
+                <span className="font-bold text-[1.8rem]">
+                  -{dashboardData?.monthly_refunds.toLocaleString()}원
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <Heading tag="h2">당월 총매출</Heading>
-                <span className="font-bold text-[1.8rem]">1,250,000,000원</span>
+                <span className="font-bold text-[1.8rem]">
+                  {dashboardData?.monthly_total_sales.toLocaleString()}원
+                </span>
               </div>
             </div>
           </div>
